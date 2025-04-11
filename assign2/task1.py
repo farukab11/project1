b74cc14a1ad4fb27cda63c4c004757e8967990d2
farukab11 <margan4521@gmail.com>
2025-04-11 22:01:33 +0530

    Add files via upload
    task 1 checks if a no. is even or odd
    task 2 calculate the sum of integers from 1 to 50

---
+++ assign2/task1.py
@@ -0,0 +1,16 @@
+# Two Numbers: Even or Odd
+
+num1 = int(input(" Enter a number: "))
+num2 = int(input(" Enter a number: "))
+
+if num1 % 2 == 0:
+    print(num1, " is an even number. ")
+else:
+    print(num1, " is an odd number. ")
+
+if num2 % 2 == 0:
+    print(num2, " is an even number. ")
+else:
+    print(num2, " is an odd number. ")
+
+
---
+++ assign2/task2.py
@@ -0,0 +1,5 @@
+# Calculate the sum of numbers from 1 to 50
+total_sum = sum(range(1, 51))
+
+# Display the final sum
+print("The sum of integers from 1 to 50 is:", total_sum)
